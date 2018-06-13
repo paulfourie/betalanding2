97a34013a1380d76c6e002dc1876b2a9debccfc9
Paul.Fourie <Paul.Fourie@SRV15192TS01>
2018-06-13 17:28:57 +0100

    weblogic install crash

--- src/main/java/com/legalservices/laa/betaLanding2/BetaLanding2Application.java
+++ src/main/java/com/legalservices/laa/betaLanding2/BetaLanding2Application.java
@@ -1,9 +1,9 @@
 package com.legalservices.laa.betaLanding2;
 
-
+/*
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+*/
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,22 +12,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /* weblogic additions */
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+/* weblogic security issue */
+/* import org.springframework.web.WebApplicationInitializer; implements WebApplicationInitializer */
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication 
-public class BetaLanding2Application extends SpringBootServletInitializer  {
+public class BetaLanding2Application extends SpringBootServletInitializer implements WebApplicationInitializer {
 
 	
-	private static final Logger logger = LoggerFactory.getLogger( BetaLanding2Application.class );   
+/*	private static final Logger logger = LoggerFactory.getLogger( BetaLanding2Application.class );   
+ */
 
 	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(BetaLanding2Application.class);
+		return application.sources( BetaLanding2Application.class);
 	}	
 	
 	public static void main(String[] args) {
-		SpringApplication.run(BetaLanding2Application.class, args);
+		SpringApplication.run( BetaLanding2Application.class, args);
 	}
   
 	
