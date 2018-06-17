f2d296eeab105839654afd84448eaf13b9670f70
Paul.Fourie <Paul.Fourie@SRV15192TS01>
2018-06-17 10:00:26 +0100

    logging patch

--- src/main/java/com/legalservices/laa/BetaLanding2/controller2.java
+++ src/main/java/com/legalservices/laa/BetaLanding2/controller2.java
@@ -9,18 +9,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
+/*
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.lang.invoke.MethodHandles;
+*/
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 @Controller
 public class controller2 extends mController {
 
     
-	final static Logger logger = LoggerFactory.getLogger( MethodHandles.lookup().lookupClass() ) ;   
+/* 	final static Logger logger = LoggerFactory.getLogger( MethodHandles.lookup().lookupClass() ) ;   */
 
+	static private Logger logger = LogManager.getLogger();
 	
 	@Autowired
 	private HttpServletRequest request;
@@ -43,7 +47,8 @@ public class controller2 extends mController {
         ModelAndView modelandView = new ModelAndView("index", params());
         
         String OssoUser = request.getHeader("Osso-User-Guid"); 
-        logger.info( "OssoUser " + OssoUser ) ;   
+        logger.error( "OssoUser " + OssoUser ) ;   
+        logger.trace( "OssoUser " + OssoUser ) ; 
         
         return modelandView;
         
